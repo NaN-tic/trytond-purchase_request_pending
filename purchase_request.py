b616972059dc832a7da2864c489124f1441bb603
Raimon Esteve <raimonesteve@gmail.com>
2022-12-07 11:54:49 +0100

    Search purchase request that has product purchase_requision create purchase requests that product is none

--- purchase_request.py
+++ purchase_request.py
@@ -82,6 +82,7 @@ class PurchaseRequest(metaclass=PoolMeta):
         requests = Request.search([
                 ('purchase_line', '=', None),
                 ('pending', '=', True),
+                ('product', '!=', None),
                 ])
         # Fetch data from existing requests
         existing_req = {}
